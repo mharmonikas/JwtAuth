889ba697398e79f49355db5c2a47d9b2b1c83004
martin <martin_n25@yahoo.com>
2019-10-08 20:58:50 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('register', 'AuthController@register');
+    Route::post('me', 'AuthController@me');
+
+});
